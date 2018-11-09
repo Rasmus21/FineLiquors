ce794b5597c8104138c9d8f562f27508c06319d2
Kristian <krso16ae@student.cbs.dk>
2018-11-09 11:20:12 +0100

    fikset kamera knap

---
+++ components/pages/CameraScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native'; 
+
+export default class CameraScreen extends React.Component {
+
+      render() {
+        return (
+            <View>
+              <Text>Hej Sørensen</Text>
+            </View>
+        );
+      }
+  };
