d3d47742dcfd9a45e598cd232182691b803bee09
Rasmus21 <raje16ab@student.cbs.dk>
2018-11-19 13:39:30 +0100

    Anmeldelsesview lavet - mangler at kunne få billede med over

--- components/pages/AddNewScreen.js
+++ components/pages/AddNewScreen.js
@@ -3,10 +3,8 @@ import {View, Button, StyleSheet, Text, TextInput} from 'react-native';
 import firebase from 'firebase';
 
 export default class AddNewScreen extends React.Component {
-
   constructor(props) {
     super(props);
-
     this.state={
       type: '',
       title: '',
@@ -14,21 +12,20 @@ export default class AddNewScreen extends React.Component {
       image: 'https://images-na.ssl-images-amazon.com/images/I/41j7-7yboXL.jpg'
     }
   }
-
   static navigationOptions = {
     title: "Tilføj anmeldelse"
   };
-
   
     writeLiquor(){
       const type = this.state.type;
       const title = this.state.title;
       const image = this.state.image;
-
+      const text = this.state.text;
       firebase.database().ref('liquors/').push({
           type,
           title,
-          image
+          image,
+          text
       }).then((data)=>{
           alert("Anmeldelse succesfuld");
       }).catch((error)=>{
@@ -40,18 +37,30 @@ export default class AddNewScreen extends React.Component {
   render() {
     return (
         <View style={styles.container}>
-           <TextInput
-          label='Liquor'
-            placeholder='Liquor'
-            value={this.state.tupe}
-            onChangeText={type => this.setState({ type })}
-          />
-          <TextInput
-            label='Flaskens Titel'
-            placeholder='Flaskens Titel'
-            value={this.state.title}
-            onChangeText={title => this.setState({ title })}
-          />
+            <View style={styles.input}>
+              <TextInput
+              label='Liquor'
+              placeholder='Liquor'
+              value={this.state.tupe}
+              onChangeText={type => this.setState({ type })}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                label='Flaskens Titel'
+                placeholder='Flaskens Titel'
+                value={this.state.title}
+                onChangeText={title => this.setState({ title })}
+              />
+            </View>
+            <View style={styles.input1}>
+              <TextInput
+                label='Anmeldelse'
+                placeholder='Anmeldelse'
+                value={this.state.text}
+                onChangeText={text => this.setState({ text })}
+              />
+            </View>
           <TextInput editable={false} value={this.state.image}/>
 
         <Text style={styles.errorTextStyle}>
@@ -77,5 +86,51 @@ const styles = StyleSheet.create({
     fontSize: 20,
     alignSelf: 'center',
     color: 'red'
-  }
+  },
+  input: {
+    alignItems: 'stretch', 
+    backgroundColor: '#98999A',
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10, 
+  },
+  input1: {
+    alignItems: 'stretch', 
+    backgroundColor: '#98999A',
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10, 
+    height: 200,
+  },
+  buttonContainer: {
+    backgroundColor: '#9FB4C4',
+    paddingVertical: 5
+  },
+  buttonText: {
+    color: '#06121C',
+    textAlign: 'center',
+    fontWeight: '700'
+  } 
 });
